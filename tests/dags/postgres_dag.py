29492d81a9b49c86c225e9c36885e8f11aad11a6
Naresh Kumar <naresh@Nareshs-MBP-2.modem>
2023-06-27 11:49:53 +1000

    postgres dag

--- tests/dags/postgres_dag.py
+++ tests/dags/postgres_dag.py
@@ -2,12 +2,13 @@ from datetime import datetime, timedelta
 from textwrap import dedent
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.providers.postgres.operators.postgres import PostgresOperato
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 with DAG(
-    "basic",
+    "postgres_dag",
     description="Postgres DAG",
     schedule_interval=timedelta(days=1),
     start_date=datetime(2021, 1, 1),
+    tags=["example"],
     catchup=False,
     default_args={"owner": "airflow",
                   "email": ["n.tensor@gmail.com"],
@@ -22,6 +23,7 @@ with DAG(
         task_id="list_tables",
         postgres_conn_id="postgres",
         sql="SELECT * FROM pg_catalog.pg_tables;",  # list all tables
+        xcom_push=True,
     )
 
     t1 >> t2
